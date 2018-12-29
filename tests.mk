634c12f02b0d07d35f36e6c3f02994dd466184a2
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2018-12-29 01:36:25 -0500

    feat: persist test-results

--- tests.mk
+++ tests.mk
@@ -209,5 +209,6 @@ deploy-tests:
 test: setup-deploy-tests lint unit-tests deploy-tests
 
 test-ci:
+	mkdir -p test-results
 	@echo "executing tests: $(shell circleci tests glob tests/unit/*.bats | circleci tests split --split-by=timings | xargs)"
-	bats --formatter bats-format-junit -e -T $(shell circleci tests glob tests/unit/*.bats | circleci tests split --split-by=timings | xargs)
+	bats --formatter bats-format-junit -e -T -o test-results $(shell circleci tests glob tests/unit/*.bats | circleci tests split --split-by=timings | xargs)
